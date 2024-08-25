585359429fdcc70cd7ab2135b0683a66f3b0ff0e
Susheel rai <97819258+mesusheel01@users.noreply.github.com>
2024-08-25 23:42:32 +0530

    Update SectionWrapper.jsx

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SectionWrapper = (props) => {
   const { children, header, title,id } = props;
   return (
-    <section id={id} className="min-h-screen flex flex-col gap-10">
+    <section id={id} className="min-h-screen flex flex-col items-center gap-10">
       <div className="bg-gray-950 py-10 flex flex-col gap-4 justify-center items-center p-4">
         <p className="uppercase font-medium">{header}</p>
         <h2 className="font-semi-bold text-3xl sm:text-4xl md:text-5xl lg-text-6xl">
